c12a4a4e3f9e0052ddf578078d82711c34ff86d1
Chris Cascioli <chriscascioli@gmail.com>
2026-01-21 14:00:13 -0500

    Added delegate/event demo

---
+++ DelegateEventDemo/Program.cs
@@ -0,0 +1,58 @@
+﻿// Chris Cascioli
+// 1/21/26
+// Example of delegates and events
+
+namespace DelegateEventDemo
+{
+    /// <summary>
+    /// An example delegate that can represent
+    /// any method that has a void return type
+    /// and accepts a single integer
+    /// </summary>
+    /// <param name="x">The only param</param>
+    delegate void MyFirstDelegate(int x);
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a variable of type "MyFirstDelegate"
+            MyFirstDelegate iHoldAMethod = ExampleMethod;
+
+            // Invoke the method we've stored
+            iHoldAMethod(7);
+			Console.WriteLine();
+
+            // --------------------
+
+            // Create the tank and deal some damage
+            Tank tank = new Tank();
+			tank.LowHealth += LowHealthCallback;
+
+            tank.TakeDamage(10);
+			tank.TakeDamage(30);
+			tank.TakeDamage(14);
+			tank.TakeDamage(7);
+			tank.TakeDamage(19);
+			tank.TakeDamage(2);
+			tank.TakeDamage(5);
+		}
+
+        /// <summary>
+        /// Method to call when a tank is low on health
+        /// </summary>
+        /// <param name="message">Tank's message</param>
+        /// <param name="number">Remaining health</param>
+		private static void LowHealthCallback(string message, int number)
+		{
+			Console.WriteLine("Teammate yells: " + message);
+			Console.WriteLine("Remaining health: " + number);
+		}
+
+		static void ExampleMethod(int number)
+        {
+			Console.WriteLine("I am the example");
+			Console.WriteLine("The number is " + number);
+        }
+    }
+}
---
+++ DelegateEventDemo/Tank.cs
@@ -0,0 +1,52 @@
+﻿// Chris Cascioli
+// 1/21/26
+// Example of a class that contains an event
+
+namespace DelegateEventDemo
+{
+	/// <summary>
+	/// Represents a method that accepts a message and a 
+	/// corresponding number
+	/// </summary>
+	delegate void ImportantMessage(string message, int number);
+
+	internal class Tank
+	{
+		// Event
+		/// <summary>
+		/// An event that fires when the tank is low on health
+		/// </summary>
+		public event ImportantMessage LowHealth;
+
+		// Fields
+		private int health;
+
+		// Constructor
+		/// <summary>
+		/// Creates a new tank at full health
+		/// </summary>
+		public Tank()
+		{
+			health = 100;
+		}
+
+		// Methods
+		/// <summary>
+		/// Applies damage to the tank and reports low health
+		/// </summary>
+		/// <param name="amount">Incoming damage</param>
+		public void TakeDamage(int amount)
+		{
+			health -= amount;
+			Console.WriteLine("Ouch!");
+
+			// Detect when we're low on health
+			// and tell anyone who cares
+			if (health < 20 && LowHealth != null)
+			{
+				// Invoke the event as if it were a method
+				LowHealth("I'm dyin' over here!", health);
+			}
+		}
+	}
+}
