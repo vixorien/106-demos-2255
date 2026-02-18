665873eedead40d1ff177b52cd377786cb57f445
Chris Cascioli <chriscascioli@gmail.com>
2026-02-18 13:53:31 -0500

    Added singleton demo

---
+++ SingletonDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿// Chris Cascioli
+// 2/18/26
+// Demo of the singleton design pattern
+
+namespace SingletonDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get my one and only instance and initialize it
+            SingletonExample.Instance.Initialize(5, "hello");
+
+            // Later, use the instance like any other object
+            SingletonExample.Instance.PrintData();
+        }
+    }
+}
---
+++ SingletonDemo/SingletonExample.cs
@@ -0,0 +1,53 @@
+﻿// Chris Cascioli
+// 2/18/26
+// Implementation of a class that uses the singleton pattern
+
+namespace SingletonDemo
+{
+	internal class SingletonExample
+	{
+		// Fields
+		private int number;
+		private string word;
+
+		// The one and only instance of
+		// the class should be static
+		private static SingletonExample instance = null!;
+
+		/// <summary>
+		/// Gets the one and only instance of this class
+		/// </summary>
+		public static SingletonExample Instance
+		{
+			get
+			{
+				if (instance == null)
+					instance = new SingletonExample();
+
+				return instance;
+			}
+		}
+
+		/// <summary>
+		/// Private constructor can only be called INSIDE the class
+		/// </summary>
+		private SingletonExample() { }
+
+		/// <summary>
+		/// Initializes (sets up) the object
+		/// </summary>
+		public void Initialize(int startNum, string startWord)
+		{
+			this.number = startNum;
+			this.word = startWord;
+		}
+
+		/// <summary>
+		/// Example of using data via methods
+		/// </summary>
+		public void PrintData()
+		{
+			Console.WriteLine("Singleton data: " + number + " " + word);
+		}
+	}
+}
